1a5db573f0b5c51a66c8b6f8bbcf8cfccbc019cb
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-06-07 00:14:38 +0900

    NewRelic導入、クラッシュ検知できる所まで完了

--- NewRelic-Introduction/NewRelic_IntroductionApp.swift
+++ NewRelic-Introduction/NewRelic_IntroductionApp.swift
@@ -6,15 +6,27 @@
 //
 
 import SwiftUI
+import NewRelic
 
 @main
 struct NewRelic_IntroductionApp: App {
     let persistenceController = PersistenceController.shared
 
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            MainView()
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        NewRelic.start(withApplicationToken:"AA640d54c34609f64256555cb0cec2dbb695a041ce-NRMA")
+
+        return true
     }
 }
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NewRelic-Introduction' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NewRelic-Introduction
+
+  pod 'NewRelicAgent'
+
+  target 'NewRelic-IntroductionTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'NewRelic-IntroductionUITests' do
+    # Pods for testing
+  end
+
+end
