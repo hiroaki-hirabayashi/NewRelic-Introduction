9667582845ef12762a61eb75771a1d05f9c9b27c
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-06-07 00:16:46 +0900

    コミット漏れ

---
+++ Pods/Target Support Files/Pods-NewRelic-Introduction-NewRelic-IntroductionUITests/Pods-NewRelic-Introduction-NewRelic-IntroductionUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewRelic_Introduction_NewRelic_IntroductionUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NewRelic_Introduction_NewRelic_IntroductionUITests
+@end
