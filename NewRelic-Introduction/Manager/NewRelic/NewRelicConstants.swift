1260ddf105e07c5e622cd7eafa0d1fda0cdc03d8
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-07-29 21:39:59 +0900

    ログ送信ロジック変更開始

--- NewRelic-Introduction/Manager/NewRelic/NewRelicConstants.swift
+++ NewRelic-Introduction/Manager/NewRelic/NewRelicConstants.swift
@@ -11,7 +11,7 @@ class NewRelicConstants {
 #if DEBUG
     // TODO: - ライセンスキー系はどこに置くか
     static let apiKey = ""
-    static let token = ""
+    static let token = "AA640d54c34609f64256555cb0cec2dbb695a041ce-NRMA"
     static let baseURL = "https://log-api.newrelic.com/log/v1"
 #else // 本番用
     static let apiKey = ""
